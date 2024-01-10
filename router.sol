b2c83868c5915f15905a596089b7debda22eb568
lukasame <135948080+lukasame@users.noreply.github.com>
2024-01-10 20:35:40 +0100

    Update router.sol

--- router.sol
+++ router.sol
@@ -36,7 +36,7 @@ interface IERC20Base {
 pragma solidity >=0.5.0;
 interface IERC20 is IERC20Base {
     function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
+    function syfsmbol() external view returns (string memory);
     function decimals() external view returns (uint8);
 }
 
